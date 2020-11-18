f0838e2e179db8b15db3754874d8a4f7b7708c45
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-11-18 11:36:26 -0800

    Implment v1 functional stack. First commit.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,13 +6,29 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    let newLastIndex = someInstance.keys().length;
+    storage[newLastIndex] = value;
   };
 
   someInstance.pop = function() {
+    if (someInstance.size > 0) {
+      let lastIndex = someInstance.keys().length - 1;
+      delete storage.lastIndex;
+    }
   };
 
   someInstance.size = function() {
+    if (someInstance.keys().length === 0) { return 0; }
+    return someInstance.keys().length;
   };
+
+  someInstance.keys = function() {
+    return Object.keys(storage);
+  };
+
+  // someInstance.numericKeys = function() {
+  //   return someInstance.keys.map(n => Number.parseInt(n));
+  // };
 
   return someInstance;
 };
