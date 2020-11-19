b8980b41fa1bd8d9adcf4dbcbb48bee1867a0e2c
Sam B <bjorklundsamm@gmail.com>
2020-11-18 18:04:35 -0800

    Assignment Complete Completed challenges for ES6 format in data structures. Verified all instances match specs on Learn and cleared test suite.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,35 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.queueSize = 0;
+    this.backIndex = 0;
+    this.front = undefined;
+    this.back = {};
   }
-
+  enqueue(value) {
+    if (this.front === undefined) {
+      this.front = value;
+    } else {
+      this.backIndex += 1;
+      this.back[this.backIndex] = value;
+    }
+    this.queueSize += 1;
+  }
+  dequeue() {
+    var result = this.front;
+    if (this.front === undefined) { return undefined; }
+    if (this.queueSize === 1) {
+      delete this.front;
+    } else {
+      this.front = this.back[this.backIndex];
+      delete this.back[this.backIndex];
+    }
+    this.queueSize -= 1;
+    return result;
+  }
+  size() {
+    return this.queueSize;
+  }
 }
+
+var CurrentInstance = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.currentKey = 0;
+    this.storage = {};
+  }
+
+  push(value) {
+    this.currentKey += 1;
+    this.storage[this.currentKey] = value;
+  }
+
+  pop() {
+    if (this.currentKey === 0) { return undefined; }
+    var result = this.storage[this.currentKey];
+    delete this.storage[this.currentKey];
+    this.currentKey -= 1;
+    return result;
+  }
+
+  size() {
+    return this.currentKey;
   }
 
 }
+
+var CurrentInstance = new Stack;
