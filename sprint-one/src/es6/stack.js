d00b3c05de83faf1cd6c4ddd38663c0aacf30ba3
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-11-18 16:11:26 -0800

    Implement es6 stack. All tests pass.

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,21 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    this.storage[this.size()] = value;
+  }
+
+  pop() {
+    var lastIndex = this.size() - 1;
+    var lastItem = this.storage[lastIndex];
+    delete this.storage[lastIndex];
+    return lastItem;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
