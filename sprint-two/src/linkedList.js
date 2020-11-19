35c0c2ded3fe19fa30b1dcfe7d7041fafdcf96ee
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-11-19 11:28:47 -0800

    Start implementation of linked list. This commit represents the head/tail index approach. Next iteration should abandon this approach and move to storing values (not indices) in head/tail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,12 +1,31 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = null; // always highest index //
+  list.tail = null; // always 0 //
 
   list.addToTail = function(value) {
+    //Adds value to the 0 index of list
+    //Shifts all previous values up but 1 index
+    //Updates list.head to equal highest index
+    if (list.head === null) {
+      list.head = 0;
+      list.tail = 0;
+    }
+    // else if (list.head === list.tail) {
+    //   list.head = list.head + 1;
+    // }
+    for (var i = list.head; i >= 0; i--) {
+      list[i + 1] = list[i];
+    }
+    list[list.tail] = Node(value);
+    list.head = list.head + 1;
+    debugger;
   };
 
   list.removeHead = function() {
+
+
+    // if we just removed the last property from list, reset head and tail to null
   };
 
   list.contains = function(target) {
