32c225c0e71374a33d63646a4e46a82881795c3f
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-11-19 17:33:40 -0800

    Implement tree. Bug hunt recursive tree contains method.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,9 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,9 +12,34 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newTree = Tree(value);
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
+  //use this
+
+  // search every child for target
+  // if no target return false
+
+  /*
+  // create function called containsValue
+    // if this.value === target
+      // return true -- we win
+    // else
+      // this.children.length > 1
+          // iterate over this.childen
+            // make recurisive call on current child
+*/
+  // return false;
+  if (this.value === target) { return true; }
+  if (this.children.length === 0) { return false; }
+  _.each(this.children, function (child, key, obj) {
+    if (child.value === target) { return true; }
+    return child.contains(target);
+  });
+  debugger;
+  return false;
 };
 
 
