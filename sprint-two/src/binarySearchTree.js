d2063f7ba1f96841e6db11a6fe5719799cc3d9f0
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-11-21 18:19:01 -0800

    Merge solo refactors: graph & binary search tree start.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,56 @@
 var BinarySearchTree = function(value) {
+  let tree = {};
+  tree.value = value;
+
+  tree.left = null;
+  tree.right = null;
+
+  tree.insert = function(insertValue) {
+    if (insertValue > tree.value) {
+      if (tree.right === null) {
+        tree.right = BinarySearchTree(insertValue);
+      }
+      tree.right.insert(insertValue);
+    }
+    if (insertValue < tree.value) {
+      if (tree.left === null) {
+        tree.left = BinarySearchTree(insertValue);
+      }
+      tree.left.insert(insertValue);
+    }
+  };
+
+  tree.contains = function(searchValue) {
+    let isPresent = false;
+    const findValue = function() {
+      /**
+       * if searchValue = current tree's value
+       *   isPresent = true
+       * if searchValue > current tree's value
+       *   tree.right.findValue(searchValue)
+       * if searchValue < current tree's value
+       *   tree.left.findValue(searchValue)
+       */
+
+      if (searchValue = this.value) { isPresent = true; }
+      if (searchValue > this.value) { this.right.findValue(searchValue); }
+      if (searchValue < this.value) { this.left.findValue(searchValue); }
+
+    };
+    findValue(searchValue);
+    return isPresent;
+  };
+  tree.depthFirstLog = function() {};
+  return tree;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+let mine = BinarySearchTree(5);
+console.log(mine);
+mine.insert(2);
+console.log(mine);
+mine.insert(3);
+console.log(mine);
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,50 +1,49 @@
 // Instantiate a new graph
 let Graph = function() {
-  this.graphStorage = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this.graphStorage[node] = [];
+  this[node] = [];
 };
 
 Graph.prototype.contains = function(node) {
-  if (this.graphStorage[node]) { return true; }
+  if (this[node]) { return true; }
   return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  for (let edge of this.graphStorage[node]) {
+  for (let edge of this[node]) {
     this.removeEdge(node, edge);
   }
-  delete this.graphStorage[node];
+  delete this[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  return this.graphStorage[fromNode].includes(toNode);
+  return this[fromNode].includes(toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  this.graphStorage[fromNode].push(toNode);
-  this.graphStorage[toNode].push(fromNode);
+  this[fromNode].push(toNode);
+  this[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  let frmnode = this.graphStorage[fromNode];
-  let tonode = this.graphStorage[toNode];
-  let fromNodeStartIndex = frmnode.indexOf(toNode);
-  let toNodeStartIndex = tonode.indexOf(fromNode);
-  frmnode.splice(fromNodeStartIndex, 1);
-  tonode.splice(toNodeStartIndex, 1);
+  let from = this[fromNode];
+  let to = this[toNode];
+  let fromNodeStartIndex = from.indexOf(toNode);
+  let toNodeStartIndex = to.indexOf(fromNode);
+  from.splice(fromNodeStartIndex, 1);
+  to.splice(toNodeStartIndex, 1);
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-  for (let node in this.graphStorage) {
-    cb(node);
+  for (let node in this) {
+    if (Array.isArray(this[node])) { cb(node); }
   }
 };
 
