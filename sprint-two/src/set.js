c968cd625febbcdb051f4d6998858cf11750a025
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-11-23 10:31:12 -0800

    Set time complexity

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -23,4 +23,13 @@ setPrototype.remove = function(item) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * * .add
+ * * * O(n)
+ *
+ * * .contains
+ * * * O(n)
+ *
+ * * .remove
+ * * * O(n)
+ *
  */
